65b79f6c93810a38e831a8534975bf7fde482009
root <root@ip-172-31-36-77.eu-central-1.compute.internal>
2020-10-20 21:41:49 +0000

    Update to attributes for sensitive data

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,5 +6,7 @@ default['docker_monitor']['docker']['version'] = '19.03'
 default['docker_monitor']['cadvisor']['version'] = 'v0.36.0'
 
 default['docker_monitor']['grafana']['version'] = '7.1.5'
+default['docker_monitor']['grafana']['grafana_admin'] = 'admin'
+default['docker_monitor']['grafana']['grafana_admin_pwd'] = 'arbabr123'
 
 default['docker_monitor']['prometheus']['version'] = 'v2.20.1'
